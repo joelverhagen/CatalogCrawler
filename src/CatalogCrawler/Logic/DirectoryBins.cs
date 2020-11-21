e477571b6d784bc7956243f708685223d4ae3b25
Joel Verhagen <joel.verhagen@gmail.com>
2020-11-21 14:04:27 -0800

    Add DirectoryBins with test

---
+++ src/CatalogCrawler/Logic/DirectoryBins.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Knapcode.CatalogCrawler
+{
+    public static class DirectoryBins
+    {
+        /// <summary>
+        /// Hashes the provided key to determine a subdirectory for it. The result directory path is based off of
+        /// multiple levels of subdirectories where each level is the hexadecimal representation of a byte from the
+        /// hash. For example, the SHA-256 hash of "newtonsoft.json" starts with "2c4cf1fb". If the depth provided is
+        /// 3, the output will be like "{baseDir}/2c/4c/f1". This is useful to limit the number of directories in any
+        /// given directory to 256.
+        /// </summary>
+        public static string GetPath(string baseDir, int depth, string key)
+        {
+            if (depth < 1 || depth > 32)
+            {
+                throw new ArgumentOutOfRangeException(nameof(depth));
+            }
+
+            using (var algorithm = SHA256.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(key);
+                var hashBytes = algorithm.ComputeHash(bytes);
+                var hashHex = BitConverter.ToString(hashBytes).Replace("-", string.Empty).ToLowerInvariant();
+                var pieces = new List<string> { baseDir };
+                for (var i = 0; i < depth; i++)
+                {
+                    pieces.Add(hashHex.Substring(2 * i, 2));
+                }
+
+                return Path.Combine(pieces.ToArray());
+            }
+        }
+    }
+}
---
+++ test/CatalogCrawler.Test/Logic/DirectoryBinsTests.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Xunit;
+
+namespace Knapcode.CatalogCrawler
+{
+    public class DirectoryBinsTests
+    {
+        [Fact]
+        public void HashesKeyForLevels()
+        {
+            var baseDir = Directory.GetCurrentDirectory();
+            var key = "newtonsoft.json";
+            var expected = Path.Combine(baseDir, "2c", "4c", "f1");
+
+            var actual = DirectoryBins.GetPath(baseDir, 3, key);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
